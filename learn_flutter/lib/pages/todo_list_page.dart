ee4b248b4ce3025dbb5410b5c218f8d0be9759c3
Rahul Dey <rahuldey832000@gmail.com>
2023-12-23 23:46:52 +0530

    create todo form

--- learn_flutter/lib/pages/todo_list_page.dart
+++ learn_flutter/lib/pages/todo_list_page.dart
@@ -8,6 +8,23 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  // Text editing controller to get an access to what thr user typed.
+  TextEditingController myController = TextEditingController();
+
+  List<String> todoList = [];
+
+  void addTodo() {
+    String input = myController.text;
+
+    if (input.trim().isNotEmpty) {
+      setState(() {
+        todoList.add(input);
+      });
+    }
+
+    myController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +34,37 @@ class _TodoListPageState extends State<TodoListPage> {
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              // Text field
+              TextField(
+                controller: myController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Write your todo...",
+                ),
+              ),
+              // add todo button
+              ElevatedButton(
+                onPressed: addTodo,
+                child: const Text("Add todo"),
+              ),
+
+              // list all todo
+              ListView.builder(
+                itemCount: todoList.length,
+                itemBuilder: (context, index) => ListTile(
+                  title: Text(index.toString()),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
